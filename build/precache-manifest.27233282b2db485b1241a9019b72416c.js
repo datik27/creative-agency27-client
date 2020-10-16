a849945ac8e2696374b44026645b429aee2a7864
datik27 <itexpert508@gmail.com>
2020-10-17 02:00:45 +0800

    done

---
+++ build/precache-manifest.27233282b2db485b1241a9019b72416c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b84592efab3b4872ed762e8132b754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c12d8ba62378aac6613",
+    "url": "/static/css/2.e96c524c.chunk.css"
+  },
+  {
+    "revision": "ef3a3053db1f62c4fad1",
+    "url": "/static/css/main.499af3b7.chunk.css"
+  },
+  {
+    "revision": "9c12d8ba62378aac6613",
+    "url": "/static/js/2.47c8370f.chunk.js"
+  },
+  {
+    "revision": "9f8702cb55408414e24afc5ac73fd822",
+    "url": "/static/js/2.47c8370f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3a3053db1f62c4fad1",
+    "url": "/static/js/main.425f178c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ea0ff53eea44d16ddf836903cdfec179",
+    "url": "/static/media/airbnb.ea0ff53e.png"
+  },
+  {
+    "revision": "7ee41b049b589263715c38e989217138",
+    "url": "/static/media/carousel-1.7ee41b04.png"
+  },
+  {
+    "revision": "b948f5f3722fd9613882802f4333f307",
+    "url": "/static/media/carousel-2.b948f5f3.png"
+  },
+  {
+    "revision": "f4f08a58f7acc521b137e369f630b6b1",
+    "url": "/static/media/carousel-3.f4f08a58.png"
+  },
+  {
+    "revision": "050ab61f89c74ed3628aa01b9e400069",
+    "url": "/static/media/carousel-4.050ab61f.png"
+  },
+  {
+    "revision": "a626e1eb4ac2f3a362695e87b62e5377",
+    "url": "/static/media/carousel-5.a626e1eb.png"
+  },
+  {
+    "revision": "679ce71dd25797a35f37633383a2fe6d",
+    "url": "/static/media/google.679ce71d.png"
+  },
+  {
+    "revision": "f2ee7bd03cdb105e194295b66b87be46",
+    "url": "/static/media/header-img.f2ee7bd0.png"
+  },
+  {
+    "revision": "92f9e827b85fdd880de610e3550c555d",
+    "url": "/static/media/loader.92f9e827.gif"
+  },
+  {
+    "revision": "e47c0531137274369eb8ad29ed81a795",
+    "url": "/static/media/logo.e47c0531.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       //get token and fetch user info
     const sessionData=sessionStorage.getItem('token')
     const token=JSON.parse(sessionData)
-    token && fetch('https://creative-agency-fullstack.herokuapp.com',{
+    token && fetch('https://git.heroku.com/glacial-brook-16714.git',{
       method:'GET',
       headers:{ 
         'Content-Type':'application/json',
